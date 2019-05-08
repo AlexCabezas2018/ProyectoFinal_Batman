cf39891fdb0b573b9a5981f086d9c00bb3c559a4
AlexCabezas2018 <alexcabezas0705@gmail.com>
2019-05-08 19:20:34 +0200

    Avance en las animaciones de Batman

--- data/Batman.js
+++ data/Batman.js
@@ -6,51 +6,72 @@ var game = function() {
         .controls()
         .touch();
 
-    Q.load(["Batman.json", "Batman.png"], function() {
-        Q.compileSheets("Batman.png", "Batman.json");
+    Q.load(["batman.png", "batman.json"], function() {
+        Q.compileSheets("batman.png", "batman.json");
+        Q.stageScene("level1");
         //Compilar los json con los sprites
     });
 
     /* Animaciones Batman */
     Q.animations('batman_anims', {
-        /*Me falta saber las animaciones básicas de Batman*/
-        static_left: {},
-        static_right: {},
-        running_left: {},
-        running_right: {},
-        jump_left: {},
-        jump_right: {},
-        crouch_left: {},
-        crouch_right: {},
-        die: {},
-        hitted_crouched: {},
-        hitted_running: {},
-        punch_crouched_left: {},
-        punch_crouched_right: {},
-        punch_jumping_left: {},
-        punch_jumping_right: {},
-        punch_left: {},
-        punch_right: {},
-        bounce_left: {},
-        bounce_right: {}
-    });
+        static_left: { frames: [0, 1, 2, 3], rate: 1/15, flip: "x" },
+        static_right: { frames: [0, 1, 2, 3], rate: 1/15, flip: false },
+        running_left: { frames: [0, 1, 2, 3, 4, 5], rate: 1/15, flip: "x" } ,
+        running_right: { frames: [0, 1, 2, 3, 4, 5], rate: 1/15, flip: false },
+        jump_left: { frames: [0, 1, 2, 3, 4], rate: 1/15, flip: "x" },
+        jump_right: { frames: [0, 1, 2, 3, 4], rate: 1/15, flip: false },
+        crouch_left: { frames: [0], rate: 1/15, flip: "x" },
+        crouch_right: { frames: [0], rate: 1/15, flip: false },
+        die: { frames: [0, 1, 2, 3, 4, 5], rate: 1/15 },
+        hitted_crouched_right: { frames: [0, 1], rate: 1/15, flip: false },
+        hitted_crounched_left: { frames: [0, 1], rate: 1/15, flip: "x" },
+        hitted_running_right: { frames: [0, 1, 2, 3, 4], rate: 1/15, flip: false },
+        hitted_running_left: { frames: [0, 1, 2, 3, 4], rate: 1/15, flip: "x" },
+        punch_crouched_left: { frames: [0, 1, 2, 3], rate: 1/15, flip: "x" },
+        punch_crouched_right: { frames: [0, 1, 2, 3], rate: 1/15, flip: false },
+        punch_jumping_left: { frames: [0, 1, 2, 3], rate: 1/15, flip: "x" },
+        punch_jumping_right: { frames: [0, 1, 2, 3], rate: 1/15, flip: false },
+        punch_left: { frames: [0, 1, 2, 3], rate: 1/15, flip: "x" },
+        punch_right: { frames: [0, 1, 2, 3], rate: 1/15, flip: false },
+        bounce_left: { frames: [0, 1, 2], rate: 1/15, flip: false },
+        bounce_right: { frames: [0, 1, 2], rate: 1/15, flip: "x" }
 
+        //TO DO: Faltan las animaciones relacionadas con el boomerang, etc
+    });
 
     Q.Sprite.extend("Batman", {
         init: function(p) {
-            this._super({
+            this._super(p, {
                 /*Coordendas, propiedades básicas*/
-
-
+                sprite: "batman_anims",
+                sheet: "batmanStaticRight",
+                x: 0,
+                y: 0, 
 
             });
-            this.add('2d, plataformerControls, animation');
+            this.add( /*2d*/ 'platformerControls, animation');
             this.play('static_right');
             //Implementar las colisiones y los disparos.
+            
+        } ,
 
-            //Comportamientos...
+        step: function(dt) {
+            if(this.p.vx > 0) {
+                this.p.sheet = "batmanRunning"
+                this.play('running_right');
+            }
+            else if(this.p.vx < 0) {
+                this.p.sheet = "batmanRunning"
+                this.play('running_left');
+            }
         }
-        //Más funciones, como el step....
 
     });
+
+
+    //Para testing
+    Q.scene("level1", function(stage) {
+        var x = stage.insert(new Q.Batman({ x: 100, y: 100 }));
+        console.log(x);
+    })
 }
